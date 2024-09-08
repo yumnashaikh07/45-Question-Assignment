ddc1da598533c9638a2605abd8b3e8c59e9c85ca
yumnashaikh07 <164198271+yumnashaikh07@users.noreply.github.com>
2024-09-08 20:15:22 +0500

    Q31 TO Q35

---
+++ 31 -35/Q 31/main.ts
@@ -0,0 +1,10 @@
+let usernames : string[] = [];
+
+if(usernames.length === 0)
+    {
+        console.log("We need to find some users!");
+    }
+else
+    {
+        //greet users
+    }
---
+++ 31 -35/Q 32/main.ts
@@ -0,0 +1,15 @@
+let current_users : string[] = ["user1","admin","user3","user4","user5"];
+let new_users : string[] =["User1","User6","User7","admin","User9"];
+new_users.forEach((newUser) => {
+    if (
+      current_users.some(
+        (currentUser) => currentUser.toLowerCase() === newUser.toLowerCase()
+      )
+    ) {
+      console.log(`${newUser} will need to enter a new username.`);
+    } else {
+      console.log(`${newUser} is available.`);
+    }
+  });
+  
+  
