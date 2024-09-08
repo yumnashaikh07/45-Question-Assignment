68381935ad5345a99742de99859374083ef581c2
yumnashaikh07 <164198271+yumnashaikh07@users.noreply.github.com>
2024-09-08 20:18:33 +0500

    Q36 TO Q40

---
+++ 36 - 40/Q 36/36.ts
@@ -0,0 +1,11 @@
+function make_shirt(size: string, message: string) {
+    console.log(`Making a ${size} t-shirt with the message ${message} printed on it`);
+  }
+  
+  make_shirt("medium", "Code is Life");
+  make_shirt("small" ,"Vibe")
+  
+  
+  
+  
+  
